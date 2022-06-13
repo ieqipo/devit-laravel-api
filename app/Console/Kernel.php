0dc25ed0a3a8c08a72bf41f8b2e14f8f5f21ef7f
ieqipo <ieqipo@gmail.com>
2022-06-13 23:53:06 +0300

    Edited Reamne Run schedule command hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 //        $schedule->command('parse:rss')->everyFiveMinutes();
-//        $schedule->command('parse:rss')->hourly();
+        $schedule->command('parse:rss')->hourly();
     }
 
     /**
